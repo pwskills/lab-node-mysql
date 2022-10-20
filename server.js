e8a4e98f23d37fe64697ac62accf4186682e60d7
Ankit Sharma <sankit945@gmail.com>
2022-10-20 23:04:55 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { default: connection } = require('./db');
+const app = express();
+
+const PORT = process.env.PUBLIC_PORT || 4000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/all', (req, res)=> {
+    connection.query('SELECT * FROM sample', (err, results, fields) => {
+        if (err) throw err;
+        res.send(results);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
