f90add1466bebd9211228a6a0e283265d576e6b9
Ankit Sharma <sankit945@gmail.com>
2022-10-21 20:33:59 +0530

    changed init file

--- init.js
+++ init.js
@@ -8,12 +8,27 @@ const connection = mysql.createConnection({
   password: 'password'
 });
 
-connection.query('CREATE DATABASE IF NOT EXISTS sample', () => {
+const dumpData = () => {
+  return new Promise(resolve => {
+    connection.query('select * from sample.sample', (err, results)=> {
+      if(results.length === 0) {
+        connection.query('insert into sample.sample values(1,"sample")', (err, res)=> {
+          console.log(err, res)
+          resolve();
+        })
+      }
+    })
+  })
+}
+
+connection.query('CREATE DATABASE IF NOT EXISTS sample', async() => {
   console.log('DB INITIALIZED')
 
   connection.query('CREATE TABLE IF NOT EXISTS sample.sample (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(255), PRIMARY KEY (id))');
 
   console.log('TABLE CREATED');
+
+  await dumpData();
 
   process.exit(0);
 });
